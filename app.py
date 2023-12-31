ae8a2c2fb174c529bcf37cacb36089005f8fd7dc
Master of None <131793324+yc-pro-123@users.noreply.github.com>
2023-12-31 11:43:51 +0530

    Update app.py
    minor update integration error

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def certificates():
     if form.validate_on_submit():
         print("hihihi")
         print(form.rollno,form.passw.data)
-        resp=requests.post(url=backendurl,params={"rno":form.rollno,"password":form.pass.data})
+        resp=requests.post(url=backendurl,params={"rno":form.rollno,"pass":form.passw.data})
         if resp.status_code==200:
             return resp.text
         
